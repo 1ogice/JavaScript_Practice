7bb3312ff33b151e8a4ea2db823e7550a75bc671
1ogice <radvancep2@gmail.com>
2022-06-25 10:18:47 +0300

    1 more task complete

--- scripts.js
+++ scripts.js
@@ -533,3 +533,14 @@ truncate("Hello World!", 20);*/
 }
 
 console.log(extractCurrencyValue("$120"));*/
+
+/*let styles = ["Джаз", "Блюз"];
+
+styles.push("Рок-н-ролл");
+
+styles[Math.floor((styles.length - 1) / 2)] = "Классика";
+
+alert(styles.shift());
+
+styles.unshift("Рэп", "Рэгги");*/
+
