2e87fc7766dd4902677b2880f03d5cb6f6b30fe0
1ogice <radvancep2@gmail.com>
2022-07-05 17:21:11 +0300

    one more

--- scripts.js
+++ scripts.js
@@ -832,3 +832,18 @@ function unique(arr) {
 }
 
 console.log(unique(list));*/
+
+/*function aclean(arr) {
+  let map = new Map();
+
+  for (let word of arr) {
+    let sorted = word.toLowerCase().split("").sort().join("");
+
+    map.set(sorted, word);
+  }
+  console.log(map);
+  return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+console.log(aclean(arr));*/
